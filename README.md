536c583426d1b2dbac7b65fd18485f2317b525eb
Ideadly-Skies <Obie.kal22@gmail.com>
2025-03-07 16:07:00 +0700

    feat: added binary_tree warm up and DFS exercise

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Sty_BinaryTrees
 repo for sty binary trees lecture and exercises
+
+## Three Rules for a Binary Tree 
+- At most 2 chilren per node.
+- 1 Root Node.
+- Exactly 1 path b/w root and any node.
+
+## Structure of Binary Tree programmatically
+- node: current value + left and right pointer to the children
---
+++ exercises/DFS.py
@@ -0,0 +1,72 @@
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+def depth_first_values(root, ls=[], isFirst=True):
+    if (root is None):
+       return ls 
+    elif (root.left is None and root.right is None): 
+      ls.append(root.val)
+      # return_ls
+      if isFirst:
+        return ls
+      return 
+
+    # recursive case 
+    ls.append(root.val)
+    depth_first_values(root.left, ls)
+    depth_first_values(root.right, ls)
+
+    # return final ls
+    return ls
+
+if __name__ == "__main__":
+    # a = Node('a')
+    # b = Node('b')
+    # c = Node('c')
+    # d = Node('d')
+    # e = Node('e')
+    # f = Node('f')        
+    # a.left = b
+    # a.right = c
+    # b.left = d
+    # b.right = e
+    # c.right = f
+
+    # #      a
+    # #    /   \
+    # #   b     c
+    # #  / \     \
+    # # d   e     f
+
+    # ls = depth_first_values(a)
+    # #   -> ['a', 'b', 'd', 'e', 'c', 'f']
+    # print(ls)
+
+    a = Node('a')
+    b = Node('b')
+    c = Node('c')
+    d = Node('d')
+    e = Node('e')
+    f = Node('f')
+    g = Node('g')
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    e.left = g
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+    #    /
+    #   g
+
+    ls = depth_first_values(a)
+    #   -> ['a', 'b', 'd', 'e', 'g', 'c', 'f']
+    print(ls)
